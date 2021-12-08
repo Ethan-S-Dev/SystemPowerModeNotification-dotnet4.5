6f81acbfc3be49e02cb98aa5838c185cac2ba8a5
Ethan Shoham <Ethan@atera.com>
2021-12-09 01:59:01 +0200

    first commit

---
+++ SystemPowerNotificationsTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.PowerNotifications;
+
+namespace SystemPowerNotificationsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SystemPowerNotifications.PowerModeChanged += SystemPowerNotifications_PowerModeChanged;
+            Console.ReadLine();
+            SystemPowerNotifications.PowerModeChanged -= SystemPowerNotifications_PowerModeChanged;
+        }
+
+        private static void SystemPowerNotifications_PowerModeChanged(object sender, PowerNotificationArgs args)
+        {
+            Console.WriteLine($"Event Fired type: {Enum.GetName(typeof(PowerBroadcastType), args.Mode)}");
+        }
+
+    }
+}
